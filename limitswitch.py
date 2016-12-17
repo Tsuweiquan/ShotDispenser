8696cb539cbb29f9cd6666e9e17c4fac69e12a1d
Tsu Wei Quan <tsuweiquan@gmail.com>
2016-12-17 20:51:49 +0800

    Update limitswitch.py

--- limitswitch.py
+++ limitswitch.py
@@ -17,9 +17,15 @@ right_stopper = 19
 gpio.setup(left_stopper, gpio.IN, pull_up_down=gpio.PUD_DOWN)  # limit switch on left
 gpio.setup(right_stopper, gpio.IN, pull_up_down=gpio.PUD_DOWN)  # limit switch on right
 
+left_input = gpio.input(left_stopper)
+right_input = gpio.input(right_stopper)
 
 print "left"
 print gpio.input(left_stopper)
+print left_input
+
 print "right"
 print gpio.input(right_stopper)
+print right_input
+
 gpio.cleanup()
